7ef5732c7d59099ad2081ab585b616ecaa6dfbae
Kelvin Ng <hoishing@gmail.com>
2024-08-06 00:47:50 +0800

    fix ci version check

--- .github/workflows/check_version.py
+++ .github/workflows/check_version.py
@@ -2,19 +2,20 @@ from pathlib import Path
 import requests, os, toml
 from packaging.version import parse
 
+# get version from pyproject.toml
+pyproject = toml.load("pyproject.toml")
+pyproject_version = pyproject["tool"]["poetry"]["version"]
+repo = pyproject["tool"]["poetry"]["repository"]
+repo_name = Path(repo).name
+
 # retrive the latest release version from the GitHub API
 response = requests.get(
-    "https://api.github.com/repos/hoishing/kTemplate/releases/latest"
+    f"https://api.github.com/repos/hoishing/{repo_name}/releases/latest"
 )
 
 # parse the response
 data = response.json()
 release_version = Path(data["html_url"]).name
-
-# get version from pyproject.toml
-pyproject = toml.load("pyproject.toml")
-
-pyproject_version = pyproject["tool"]["poetry"]["version"]
 
 if parse(pyproject_version) > parse(release_version):
     output_file = os.getenv("GITHUB_OUTPUT")
